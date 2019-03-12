d98802ba3cf19017b3867226adb7509471d4c9b7
Marvin-Magmodules <marvin@magmodules.eu>
2019-03-12 12:11:11 +0100

    Added Price Check on Custom fields in selftest

--- Helper/Selftest.php
+++ Helper/Selftest.php
@@ -12,6 +12,7 @@ use Magmodules\Channable\Helper\General as GeneralHelper;
 use Magmodules\Channable\Helper\Feed as FeedHelper;
 use Magmodules\Channable\Helper\Order as OrderHelper;
 use Magmodules\Channable\Helper\Item as ItemHelper;
+use Magmodules\Channable\Helper\Source as SourceHelper;
 use Magmodules\Channable\Model\Generate as GenerateModel;
 use Magento\Cron\Model\Schedule;
 
@@ -91,8 +92,9 @@ class Selftest extends AbstractHelper
         foreach ($configData as $storeId => $feedData) {
             if ($feedData['status']) {
                 $stores[] = [
-                    'name'       => $feedData['name'],
-                    'qty'        => $this->generateModel->getSize($storeId),
+                    'id'           => $storeId,
+                    'name'         => $feedData['name'],
+                    'qty'          => $this->generateModel->getSize($storeId),
                     'last_fetched' => $feedData['last_fetched'],
                 ];
                 $names[] = $feedData['name'];
@@ -105,7 +107,8 @@ class Selftest extends AbstractHelper
 
             foreach ($stores as $store) {
                 if ($store['qty'] == -1) {
-                    $msg = __('Storeview "%1": could not calculate number of product, please check the Channable logfile.', $store['name']);
+                    $msg = __('Storeview "%1": could not calculate number of product, please check the Channable logfile.',
+                        $store['name']);
                     $results[] = $this->getFail($msg);
                 } elseif ($store['qty'] == 0) {
                     $msg = __('Storeview "%1": no products found, please check the filter settings', $store['name']);
@@ -114,7 +117,6 @@ class Selftest extends AbstractHelper
                     $msg = __('Storeview "%1": found ~%2 items with current settings/filters. <small><i>Note:</i> This quantity is an indication, actual item quantity can vary due to product ralations.</small>', $store['name'], $store['qty']);
                     $results[] = $this->getPass($msg);
                 }
-
                 if (!empty($store['last_fetched'])) {
                     $msg = __('Storeview "%1": last fetched %2', $store['name'], $store['last_fetched']);
                     $results[] = $this->getPass($msg);
@@ -122,78 +124,16 @@ class Selftest extends AbstractHelper
                     $msg = __('Storeview "%1": not yet fetched, please check the Connection with your Channable account', $store['name']);
                     $results[] = $this->getNotice($msg);
                 }
+
+                $priceAttributes = $this->checkPriceAttributes($store['id']);
+                if (!empty($priceAttributes)) {
+                    $msg = __('Storeview "%1": found price attributes (%2) in extra fields. This is not recommended as prices are created dynamically based on multiple factors like VAT, Price Rules, etc.', $store['name'], implode(',', $priceAttributes));
+                    $results[] = $this->getNotice($msg);
+                }
             }
         } else {
             $msg = __('No Storeviews(s) Enabled');
             $results[] = $this->getFail($msg, '#feed-enable');
-        }
-
-        return $results;
-    }
-
-    /**
-     *
-     */
-    public function runApiTests()
-    {
-
-        $enabled = $this->orderHelper->getEnabled();
-        if ($enabled) {
-            $msg = __('Order Import: Enabled');
-            $results[] = $this->getPass($msg);
-        } else {
-            $msg = __('Order Import: Not Enabled');
-            $results[] = $this->getFail($msg, '#order-import');
-        }
-
-        $stores = [];
-        $names = [];
-
-        $configData = $this->itemHelper->getConfigData();
-        foreach ($configData as $storeId => $itemData) {
-            if ($itemData['enable']) {
-                $stores[] = [
-                    'name'       => $itemData['name'],
-                    'webhook'    => $itemData['webhook']
-                ];
-                $names[] = $itemData['name'];
-            }
-        }
-
-        if (!empty($stores)) {
-            $msg = __('Item Update: Enabled Storeviews(s): %1', implode(', ', $names));
-            $results[] = $this->getPass($msg);
-            foreach ($stores as $store) {
-                if (empty($store['webhook'])) {
-                    $msg = __('Item Update: There is no webhook set for Storeview "%1"', $store['name']);
-                    $results[] = $this->getFail($msg, '#item-update');
-                }
-            }
-        } else {
-            $msg = __('Item Update: No Storeviews(s) Enabled');
-            $results[] = $this->getFail($msg, '#item-update-enable');
-        }
-
-        $cron = $this->itemHelper->isCronEnabled();
-        if ($cron) {
-            $msg = __('Item Update: The Cron is Enabled');
-            $results[] = $this->getPass($msg);
-        } else {
-            $msg = __('Item Update: The Cron is Not Enabled');
-            $results[] = $this->getFail($msg, '#item-update-cronjob');
-        }
-
-        if ($lastRun = $this->checkMagentoCron()) {
-            if ((time() - strtotime($lastRun)) > 3600) {
-                $msg = __('Magento cron not seen in last hour (last: %s)', $lastRun);
-                $result[] = $this->getFail($msg, '#cronjob');
-            } else {
-                $msg = __('Magento cron seems to be running (last: %s)', $lastRun);
-                $result[] = $this->getPass($msg);
-            }
-        } else {
-            $msg = __('Magento cron not setup');
-            $result[] = $this->getFail($msg, '#cronjob');
         }
 
         return $results;
@@ -265,6 +205,94 @@ class Selftest extends AbstractHelper
     public function getNotice($msg, $link = null)
     {
         return $this->getHtmlResult($msg, 'notice', $link);
+    }
+
+    /**
+     * @param $storeId
+     *
+     * @return array
+     */
+    public function checkPriceAttributes($storeId)
+    {
+        $priceAttributes = ['price', 'special_price'];
+        $foundAttributes = [];
+        if ($extraFields = $this->generalHelper->getStoreValueArray(SourceHelper::XPATH_EXTRA_FIELDS, $storeId)) {
+            foreach ($extraFields as $attribute) {
+                if (!empty($attribute['attribute']) && in_array($attribute['attribute'], $priceAttributes)) {
+                    $foundAttributes[] = $attribute['attribute'];
+                }
+            }
+        }
+
+        return $foundAttributes;
+    }
+
+    /**
+     *
+     */
+    public function runApiTests()
+    {
+
+        $enabled = $this->orderHelper->getEnabled();
+        if ($enabled) {
+            $msg = __('Order Import: Enabled');
+            $results[] = $this->getPass($msg);
+        } else {
+            $msg = __('Order Import: Not Enabled');
+            $results[] = $this->getFail($msg, '#order-import');
+        }
+
+        $stores = [];
+        $names = [];
+
+        $configData = $this->itemHelper->getConfigData();
+        foreach ($configData as $storeId => $itemData) {
+            if ($itemData['enable']) {
+                $stores[] = [
+                    'name'    => $itemData['name'],
+                    'webhook' => $itemData['webhook']
+                ];
+                $names[] = $itemData['name'];
+            }
+        }
+
+        if (!empty($stores)) {
+            $msg = __('Item Update: Enabled Storeviews(s): %1', implode(', ', $names));
+            $results[] = $this->getPass($msg);
+            foreach ($stores as $store) {
+                if (empty($store['webhook'])) {
+                    $msg = __('Item Update: There is no webhook set for Storeview "%1"', $store['name']);
+                    $results[] = $this->getFail($msg, '#item-update');
+                }
+            }
+        } else {
+            $msg = __('Item Update: No Storeviews(s) Enabled');
+            $results[] = $this->getFail($msg, '#item-update-enable');
+        }
+
+        $cron = $this->itemHelper->isCronEnabled();
+        if ($cron) {
+            $msg = __('Item Update: The Cron is Enabled');
+            $results[] = $this->getPass($msg);
+        } else {
+            $msg = __('Item Update: The Cron is Not Enabled');
+            $results[] = $this->getFail($msg, '#item-update-cronjob');
+        }
+
+        if ($lastRun = $this->checkMagentoCron()) {
+            if ((time() - strtotime($lastRun)) > 3600) {
+                $msg = __('Magento cron not seen in last hour (last: %s)', $lastRun);
+                $result[] = $this->getFail($msg, '#cronjob');
+            } else {
+                $msg = __('Magento cron seems to be running (last: %s)', $lastRun);
+                $result[] = $this->getPass($msg);
+            }
+        } else {
+            $msg = __('Magento cron not setup');
+            $result[] = $this->getFail($msg, '#cronjob');
+        }
+
+        return $results;
     }
 
     /**
