9f7e19fcba1ab17e23199b909806b53cf4b193cd
Marvin-Magmodules <marvin@magmodules.eu>
2022-03-08 20:54:59 +0100

    Transfer transaction fee from quote to order #146

--- Block/Adminhtml/Order/Totals.php
+++ Block/Adminhtml/Order/Totals.php
@@ -8,63 +8,37 @@ declare(strict_types=1);
 namespace Magmodules\Channable\Block\Adminhtml\Order;
 
 use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Framework\Registry;
-use Magento\Framework\View\Element\Template\Context;
-use Magento\Quote\Api\CartRepositoryInterface;
 use Magento\Sales\Block\Adminhtml\Order\Totals as MagentoTotals;
-use Magento\Sales\Helper\Admin;
 
 /**
  * Class to add Marketplace Transaction Fee
  */
 class Totals extends MagentoTotals
 {
-    /**
-     * @var CartRepositoryInterface
-     */
-    private $quote;
 
     /**
-     * @param Context $context
-     * @param Registry $registry
-     * @param Admin $adminHelper
-     * @param CartRepositoryInterface $cartRepositoryInterface
-     * @param array $data
-     */
-    public function __construct(
-        Context $context,
-        Registry $registry,
-        Admin $adminHelper,
-        CartRepositoryInterface $cartRepositoryInterface,
-        array $data = []
-    ) {
-        $this->quote = $cartRepositoryInterface;
-        parent::__construct($context, $registry, $adminHelper, $data);
-    }
-
-    /**
-     * Initialize all order totals relates with tax
+     * Add Marketplace Transaction Fee to totals
      *
      * @return Totals
      * @throws NoSuchEntityException
      */
     public function initTotals()
     {
-        $quoteId = $this->getOrder()->getQuoteId();
-        $quote = $this->quote->get($quoteId);
         $parent = $this->getParentBlock();
-        $this->_order = $parent->getOrder();
-        if ($quote->getTransactionFee() > 0) {
-            $fee = new \Magento\Framework\DataObject(
-                [
-                    'code' => 'transaction_fee',
-                    'strong' => false,
-                    'value' => $quote->getTransactionFee(),
-                    'label' => __('Marketplace Transaction Fee'),
-                ]
-            );
+        $order = $parent->getOrder();
 
-            $parent->addTotal($fee, 'transaction_fee');
+        if ($order->getTransactionFee() != 0) {
+            $parent->addTotal(
+                new \Magento\Framework\DataObject(
+                    [
+                        'code'   => 'transaction_fee',
+                        'strong' => false,
+                        'value'  => $order->getTransactionFee(),
+                        'label'  => __('Marketplace Transaction Fee'),
+                    ]
+                ),
+                'transaction_fee'
+            );
         }
 
         return $this;
