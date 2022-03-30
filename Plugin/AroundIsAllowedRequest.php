db1a95e0e75be6d807ffc4f15116af16dd1f6b00
Marvin-Magmodules <marvin@magmodules.eu>
2022-03-30 20:26:23 +0200

    Bypass Amasty Shop By SEO url parser

---
+++ Plugin/AroundIsAllowedRequest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright © Magmodules.eu. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magmodules\Channable\Plugin;
+
+use Magento\Framework\App\RequestInterface;
+
+/**
+ * Bypass Amasty Shop By SEO url parser.
+ */
+class AroundIsAllowedRequest
+{
+
+    /**
+     * @param                  $subject
+     * @param \Closure         $proceed
+     * @param RequestInterface $request
+     * @param bool             $allowEmptyModuleName
+     **
+     * @return bool
+     */
+    public function aroundIsAllowedRequest(
+        $subject,
+        \Closure $proceed,
+        RequestInterface $request,
+        $allowEmptyModuleName = false
+    ) {
+        $identifier = ltrim($request->getOriginalPathInfo(), '/');
+        if (strpos($identifier, 'channable/') !== FALSE) {
+            return false;
+        }
+
+        $proceed($request, $allowEmptyModuleName);
+    }
+}
